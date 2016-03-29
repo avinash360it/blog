fb0c923970c6a1ae4ea4f99869f38df58e435f25
root <root@bss401dt023.betasoftsystems.com>
2016-03-29 15:04:27 +0530

    second commit

--- routes/register.js
+++ routes/register.js
@@ -2,23 +2,23 @@
  * Created by root on 29/3/16.
  */
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({ dest: '../images/profile_pic/'}).single('profilePic');
+//var upload = multer({ dest: '/public/images/profile_pic/'});
+console.log(path.join(__dirname,'../public/uploads'));
+//var upload = multer({ dest: __dirname + '../public/uploads/'});
+var upload = multer({ dest: path.join(__dirname,'../public/uploads')});
 var User = require('../public/javascripts/models/user');
 
-router.post('/process', function (req,res,next) { //upload.single('profilePic')
-    upload(req,res, function (err) {
-        if(err){
-            console.log(err);
-        }
-    });
+router.post('/process',upload.single('profilePic'), function (req,res,next) {
+//router.post('/process',upload, function (req,res,next) {
     //console.log(req);
     var user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-        profilePic: '',//req.file.path,
+        profilePic: req.file.path,
         createdAt: new Date()
     }).save(function(err){
             if(err){
